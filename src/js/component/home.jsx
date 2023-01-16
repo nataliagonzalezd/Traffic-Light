70d5943dd8def5ede4e93d795fd0b6e4a4ab935e
nataliagonzalezd <nataliagd11@hotmail.com>
2023-01-16 23:58:56 +0000

    My first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, {useState} from "react"; //1. importar el hook useState
+
+//use state = usar estado;
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	//    valor inicial, actualiza el valor inicial
+//    const [nombre,setNombre]= useState("Manzana")
+//    const [seguidores,setSeguidores]= useState(0)
+   const [color1,setColor1]= useState("");
+
+//    function cambiarNombre() {
+// 	setNombre("Pera")
+//    }
+
+//    function follow() {
+// 	//             0  + 1 = 1
+// 	setSeguidores(seguidores+1)
+
+//    }
+
+//    useState()//===> [variable,function]
+
+	//funcionalidades del componente
+    //  let nombre = "Rosinni";
+	//  variables, constates, funciones
+	function encenderRojo() {
+
+		if(color1 === "btn-danger"){
+			setColor1("")
+		}else{
+		setColor1("btn-danger")
+	}
+}
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center d-flex flex-column">
+			<button className={"btn "+color1} onClick={encenderRojo}>Rojo</button>
+			<button>Amarillo</button>
+			<button>Verde</button>
 		</div>
 	);
+
 };
 
 export default Home;
+
